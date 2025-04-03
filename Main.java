1b1429fc0f2a9517c1ba94d35391acb525d9e50e
Carlos Miguel Marquez Suela <126050848+carlossuela576@users.noreply.github.com>
2025-04-03 22:22:18 +0700

    implement method
    update main

--- Main.java
+++ Main.java
@@ -2,32 +2,31 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    int usSys;
-    int usDias;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        getCategory pt = new getCategory();
+
         while(true){
             try {
-                promptsInputs();
+                System.out.println("BLOOD PRESSURE ANNALYZER");
+                System.out.println("-------------------------");
+                System.out.println("Please enter the given");
+                System.out.print("Systolic: ");
+                int usSys = sc.nextInt();
+                sc.next();
+                System.out.print("Diastolic: ");
+                int usDias = sc.nextInt();
+                sc.next();
+
+                pt.decisionCateg(usSys, usDias);
+                pt.classifyCateg();
             } 
             catch (InputMismatchException e) {
+                System.out.println();
                 System.out.println("Something went wrong");
                 System.out.println("Please enter correct inputs");
-                System.out.println(e);
-                System.out.println();
             }
         }
         
-    }
-    static void promptsInputs(){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("BLOOD PRESSURE ANNALYZER");
-        System.out.println("-------------------------");
-        System.out.println("Please enter the given");
-        System.out.print("Systolic: ");
-        int usSys = sc.nextInt();
-        System.out.print("Diastolic: ");
-        int usDias = sc.nextInt();
     }
 }
