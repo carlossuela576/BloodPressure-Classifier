395dc634fcdb07fbbf634f297f7879e798022f0c
Carlos Miguel Marquez Suela <126050848+carlossuela576@users.noreply.github.com>
2025-04-03 22:25:27 +0700

    fix error handling

--- Main.java
+++ Main.java
@@ -6,26 +6,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         getCategory pt = new getCategory();
 
-        while(true){
-            try {
+        try {
+            while (true) { 
                 System.out.println("BLOOD PRESSURE ANNALYZER");
                 System.out.println("-------------------------");
                 System.out.println("Please enter the given");
                 System.out.print("Systolic: ");
                 int usSys = sc.nextInt();
-                sc.next();
                 System.out.print("Diastolic: ");
                 int usDias = sc.nextInt();
-                sc.next();
 
                 pt.decisionCateg(usSys, usDias);
                 pt.classifyCateg();
-            } 
-            catch (InputMismatchException e) {
-                System.out.println();
-                System.out.println("Something went wrong");
-                System.out.println("Please enter correct inputs");
             }
+        } 
+        catch (InputMismatchException e) {
+            System.out.println();
+            System.out.println("Something went wrong");
+            System.out.println("Please enter correct inputs");
         }
         
     }
