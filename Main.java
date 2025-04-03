5a47657ef8fd02f13534f37c9818d219c40490ac
Carlos Miguel Marquez Suela <126050848+carlossuela576@users.noreply.github.com>
2025-04-03 21:30:27 +0700

    Initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    int usSys;
+    int usDias;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            try {
+                promptsInputs();
+            } 
+            catch (InputMismatchException e) {
+                System.out.println("Something went wrong");
+                System.out.println("Please enter correct inputs");
+                System.out.println(e);
+            }
+        }
+        
+    }
+    static void promptsInputs(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("BLOOD PRESSURE ANNALYZER");
+        System.out.println("-------------------------");
+        System.out.println("Please enter the given");
+        System.out.print("Systolic: ");
+        int usSys = sc.nextInt();
+        System.out.print("Diastolic: ");
+        int usDias = sc.nextInt();
+    }
+}
